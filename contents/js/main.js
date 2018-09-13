875855459da5fcb8d5be856a14a27cd83dbd062d
김지혜 <wisterra@naver.com>
2018-09-14 03:34:07 +0900

    slider add, category link add
    slider add, category link add

---
+++ contents/js/main.js
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------
+// main
+//---------------------------------------------------
+
+    /* slider */
+    $(function () {
+        sliderActon();
+    });
+
+    function sliderActon() {
+        var $slider = $('.visual-con');
+
+        // slider option
+        var slickOptions = {
+            //lazyLoad: 'ondemand',
+            dots: true,
+            prevArrow: $('.slider-prev'),
+            nextArrow: $('.slider-next')
+        }
+        $slider.slick(slickOptions);
+    }
