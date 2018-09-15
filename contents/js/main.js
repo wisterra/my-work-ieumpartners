a72ed7d0456b5b152226ca02f1ded8b348da1e65
김지혜 <wisterra@naver.com>
2018-09-16 03:04:13 +0900

    메인페이지 상단메뉴 hover 보완
    메인페이지 상단메뉴 hover 보완

--- contents/js/main.js
+++ contents/js/main.js
@@ -2,20 +2,35 @@
 // main
 //---------------------------------------------------
 
-    /* slider */
-    $(function () {
-        sliderActon();
-    });
+/* slider */
+$(function () {
+    sliderActon();
+});
 
-    function sliderActon() {
-        var $slider = $('.visual-con');
+function sliderActon() {
+    var $slider = $('.visual-con');
 
-        // slider option
-        var slickOptions = {
-            //lazyLoad: 'ondemand',
-            dots: true,
-            prevArrow: $('.slider-prev'),
-            nextArrow: $('.slider-next')
-        }
-        $slider.slick(slickOptions);
+    // slider option
+    var slickOptions = {
+        //lazyLoad: 'ondemand',
+        dots: true,
+        prevArrow: $('.slider-prev'),
+        nextArrow: $('.slider-next')
     }
+    $slider.slick(slickOptions);
+}
+
+/* news ticker */
+jQuery(function(){
+	var $ = jQuery;
+  jQuery('.btn').addClass('pointer').click(function () {
+   var ih = $(this).index() == 0 ? -18 : 18; //위아래로 움직이는 px 숫자
+   var obj = $('.recommend_list');
+   obj.animate({ scrollTop:obj.scrollTop() + ih }, 100);
+  });
+});
+jQuery(function(){
+  function fn_article(container,btn, auto){}
+});
+fn_article3('notice5','bt5',true);
+
